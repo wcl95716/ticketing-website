9c7bbd682bf5707d0ade0cefa3037794bb6eba2a
Panda <panda@MacBook-Pro.local>
2023-10-28 02:23:33 +0800

    add python files

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+
+# 示例用法
+from backend.models.ticket import Ticket
+from backend.models.types import Priority, TicketStatus
+
+
+ticket = Ticket(1, "问题报告", "2023-10-28 10:00:00", TicketStatus.NEW, Priority.HIGHEST, "用户A", None, "报告问题", None)
+ticket.add_history("支持团队", TicketStatus.IN_PROGRESS, "支持团队", "2023-10-28 10:15:00")
+ticket.add_history("用户A", TicketStatus.COMPLETED, "支持团队", "2023-10-28 11:00:00")
+
+print(ticket.ticket_id)
