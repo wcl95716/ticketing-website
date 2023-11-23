59bddc002b42270242a883f749b9ad31211ae6b6
Your Name <you@example.com>
2023-11-23 23:08:42 +0800

    update

--- backend/src/models/wechat_bot/types/chat_action_function.py
+++ backend/src/models/wechat_bot/types/chat_action_function.py
@@ -14,8 +14,9 @@ class ChatActionsEnum(Enum):
     
 class ChatActionFunctionFactory:
     
-    url = "http://47.116.201.99:4000/user_chat_page"
+    page_url = "http://47.116.201.99:4000/user_chat_page"
     # ticket_id=2023-11-17-211224191561&customer_id=%E4%BD%A0%E6%98%AF
+    @staticmethod
     def add_ticket_to_website(ticket_record: dict = None) -> dict:
         url = 'http://47.116.201.99:8001/test/add_ticket'
         response = requests.post(url, json=ticket_record)
@@ -24,6 +25,7 @@ class ChatActionFunctionFactory:
             # 如果响应状态码为200，表示成功添加工单
             ticket_info = response.json()
             # print("Success:", ticket_info)
+            
             
             local_logger.logger.info("add_ticket_to_website ticket_info : %s", ticket_info)
             return ticket_info
@@ -34,10 +36,34 @@ class ChatActionFunctionFactory:
             return None
     pass
 
+    def add_ticket_init_chat(ticket_record: dict = None) -> dict:
+        url = 'http://47.116.201.99:8001/test/add_chat_record'
+        ticket_id = ticket_record["ticket_id"]
+        
+            # 生成随机的消息ID、工单ID、发送者、消息内容和消息时间
+        message_id = ""
+        sender = '系统消息'  
+        content = "你好，有什么可以帮你的?可以在此留言"
+
+        message_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message_type = 0
+
+        chatMessage = {
+            "message_id": message_id,
+            "ticket_id": ticket_id,
+            "sender": sender,
+            "content": content,
+            "message_time": message_time,
+            "message_type": message_type
+        }
+        response = requests.post(url, json=chatMessage)
+        
+        
+
 
     @staticmethod
     def get_work_order_link(ticket_id: str ,customer_id ) -> str:
-        return f"@{customer_id} 工单通知  {ChatActionFunctionFactory.url}?ticket_id={ticket_id}&customer_id={customer_id}"
+        return f"@{customer_id} 工单通知  {ChatActionFunctionFactory.page_url}?ticket_id={ticket_id}&customer_id={customer_id}"
     
     @staticmethod
     def work_order_create(group_id, message: tuple = None) -> tuple[str]:
@@ -62,7 +88,7 @@ class ChatActionFunctionFactory:
         }
         
         ticket_response = ChatActionFunctionFactory.add_ticket_to_website(ticket_data)
-        
+        ChatActionFunctionFactory.add_ticket_init_chat(ticket_response)
         local_logger.logger.info("work_order_create ticket_response : %s", ticket_response)
         if ticket_response is None:
             return None
