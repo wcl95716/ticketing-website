e307e86e987cfdbc8b52bb81927579eee88cbcda
Your Name <you@example.com>
2023-11-24 12:22:04 +0800

    update

--- front-end/src/pages/Chatpage/index.tsx
+++ front-end/src/pages/Chatpage/index.tsx
@@ -280,14 +280,21 @@ const PChart = () => {
             <div className={Style['chat-input']} style={{ marginTop: 'auto', paddingBottom: '15px', width: 'auto' }}>
                 <div style={{ display: 'flex', width: 'auto', marginBottom: '30px' }}>
                     <Input
-                        // inputMode="none" // 添加这一行
+                        inputMode="text"
                         style={{ flex: 1, marginRight: '2vh' }}
                         placeholder="请输入聊天内容"
-                        suffix={<Button type='primary' onClick={
+                        suffix={
+                           <div>
+                                                      <Button type='primary' onClick={
                             () => {
                                 handleSendMessage();
                             }
                         }>发送</Button>
+                                            {/* <Upload {...props} showUploadList={false} >
+                        <Button style={{ flex: '0 0 auto', marginTop: '5px' }} shape="circle" icon={<PlusOutlined style={{ color: 'grey' }} />} />
+                    </Upload> */}
+                           </div>
+
                         
                      }
                         value={newMessage}
