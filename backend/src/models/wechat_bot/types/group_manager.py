16235540215bc493e703cd6c6bbed9ec8dedba2b
Your Name <you@example.com>
2023-11-26 03:18:54 +0800

    update

--- backend/src/models/wechat_bot/types/group_manager.py
+++ backend/src/models/wechat_bot/types/group_manager.py
@@ -58,7 +58,7 @@ class GroupManager:
         local_logger.logger.info(f"process_group_tasks {process_group_list} ")
         for group in self.group_manager_list:
             # 如果不在处理的群聊列表中，则跳过
-            if group.group_id not in process_group_list and self.is_init:
+            if self.is_init and not any(group.group_id.startswith(prefix) for prefix in process_group_list):
                 local_logger.logger.info(f"跳过群聊 {group.group_id}")
                 continue
             group_id = group.group_id
