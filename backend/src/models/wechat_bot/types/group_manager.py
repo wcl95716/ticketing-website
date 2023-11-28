f1b27fb38bf295d8b0377d25136512a25460cdf6
Your Name <you@example.com>
2023-11-28 22:14:26 +0800

    update

--- backend/src/models/wechat_bot/types/group_manager.py
+++ backend/src/models/wechat_bot/types/group_manager.py
@@ -100,7 +100,7 @@ def test_group_manager():
         try:
             group_list = get_group_list()
             # 获取group_list 前三个群聊
-            group_list = group_list[:1]
+            group_list = group_list[:2]
             group_manager.process_group_tasks(process_group_list=group_list)
         except Exception as e:
             local_logger.logger.info(f"处理群聊任务时发生错误：{str(e)}")
