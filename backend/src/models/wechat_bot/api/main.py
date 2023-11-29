70850c37928111c367b921f0458d11c33fa58817
Your Name <you@example.com>
2023-11-29 18:14:18 +0800

    update

--- backend/src/models/wechat_bot/api/main.py
+++ backend/src/models/wechat_bot/api/main.py
@@ -41,7 +41,7 @@ def test_group_manager():
         try:
             group_list = get_group_list()
             # 获取group_list 前三个群聊
-            group_list = group_list[1:3]
+            group_list = group_list[:3]
             group_manager.process_group_tasks(process_group_list=group_list)
         
             fix_online_tasks()
