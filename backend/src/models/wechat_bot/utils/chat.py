7a78fa7f96682ddf2cd9e79d318c42f7eb064874
Your Name <you@example.com>
2023-11-29 19:10:42 +0800

    update

--- backend/src/models/wechat_bot/utils/chat.py
+++ backend/src/models/wechat_bot/utils/chat.py
@@ -26,7 +26,6 @@ def get_group_list () -> list[str]:
     pass
 
 def get_chat_messages(who:str) -> list:
-    group_list = wx.GetSessionList()  # 获取会话列表
     wx.ChatWith(who)  # 打开`who`聊天窗口
     return wx.GetAllMessage # 获取所有消息
     pass 
