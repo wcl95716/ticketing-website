e8d9dae93c04a4df0df15e70bb41cb9a85df3c03
Your Name <you@example.com>
2023-12-03 23:20:19 +0800

    update

--- backend/src/models/wechat_robot_online/types/robot_task_type.py
+++ backend/src/models/wechat_robot_online/types/robot_task_type.py
@@ -1,11 +1,19 @@
 
+from enum import Enum
+
+
+class RobotTaskType(Enum):
+    TEXT_TYPE = 0
+    IMAGE_TYPE = 1
+    VIDEO_TYPE = 2
+    pass 
 
 class RobotTask:
     # 任务类型
     # 发送的人
     # 发送的内容
     
-    def __init__(self, to_user, content,task_type = 0,):
+    def __init__(self, to_user, content,task_type:int = RobotTaskType.TEXT_TYPE.value,):
         self.task_type = task_type
         self.to_user = to_user
         self.content = content
